6b349be58a648472128388bd54a11a4404597838
Felipe Cordeiro Ramos <felipecordeiro4@gmail.com>
2020-10-12 20:01:16 -0300

    primeiro commit

---
+++ src/main/java/com/fcr/integracaopetzbackrest/IntegracaoPetzBackRestApplication.java
@@ -0,0 +1,13 @@
+package com.fcr.integracaopetzbackrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoPetzBackRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoPetzBackRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcr/integracaopetzbackrest/IntegracaoPetzBackRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcr.integracaopetzbackrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoPetzBackRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
